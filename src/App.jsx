d2377344087c55446875b533b8f808251331944d
rucNick <ruc36@pitt.edu>
2025-11-20 21:52:02 -1000

    fix the phone view and the project styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ScrollToTop from './components/ScrollToTop';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -27,6 +28,7 @@ import './styles/volunteer.css';
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
